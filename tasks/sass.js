19ae606b9977b5e2689defd0bed402a6a25a301a
Nick Harris <nick@pixelenvy.co.uk>
2014-11-23 17:30:24 +0000

    Removed deprecated souremaps flag

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,8 +1,7 @@
 module.exports = {
   dist: {
     options: {
-      style: 'expanded',
-      sourcemap: true
+      style: 'expanded'
     },
     files: [{
       '<%= package.dest_temp %>/<%= package.assets %>/css/web.designer.css': '<%= package.src %>/<%= package.assets %>/css/main.scss'
