8258aec4dc80caaf701bcbf28e7c6b6310ad73c0
Nick Harris <nick@pixelenvy.co.uk>
2015-02-26 17:29:08 +0000

    Make SCSS adhere to scss linting rules

--- tasks/scsslint.js
+++ tasks/scsslint.js
@@ -1,6 +1,8 @@
 module.exports = {
     allFiles: [
-        '<%= package.paths.src %>/<%= package.paths.assets.dev %>/css/**/*.scss'
+        '<%= package.paths.src %>/<%= package.paths.assets.dev %>/css/**/*.scss',
+        '!<%= package.paths.src %>/<%= package.paths.assets.dev %>/css/**/normalize.scss',
+        '!<%= package.paths.src %>/<%= package.paths.assets.dev %>/css/**/type-scale.scss'
     ],
     options: {
         bundleExec: false,
