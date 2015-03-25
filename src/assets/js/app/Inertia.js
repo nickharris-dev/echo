9feea76b70e95a0ed7fdf72c4f63567923065c21
Nick Harris <nick@pixelenvy.co.uk>
2015-03-25 23:24:41 +0000

    Drag scrolling working

--- src/assets/js/app/Inertia.js
+++ src/assets/js/app/Inertia.js
@@ -1,4 +1,4 @@
-define(['require', 'merge'], function(require, merge){
+define(['require', 'merge', 'csssupport'], function(require, merge, csssupport){
   var Inertia = function(elem, obj) {
     var defaults = {};
     defaults.direction = 'vertical'; // horizontal || vertical
@@ -43,20 +43,68 @@ define(['require', 'merge'], function(require, merge){
       self.element.addEventListener('mouseup', function(event){
         self.release.call(self,event);
       });
-
-      console.log(self);
     },
 
     on: function(event) {
+      var self = this;
 
+      self.pressed = true;
+      self.reference = self.pos(event);
+
+      event.preventDefault();
+      event.stopPropagation();
+      return false;
     },
 
     release: function(event) {
+      var self = this;
 
+      self.pressed = false;
+
+      event.preventDefault();
+      event.stopPropagation();
+      return false;
     },
 
     drag: function(event) {
+      var self = this;
+      var n, delta;
 
+      if (self.pressed) {
+          n = self.pos(event);
+          delta = self.reference - n;
+          if (delta > 2 || delta < -2) {
+              self.reference = n;
+              self.scroll(self.offset + delta);
+          }
+      }
+
+      event.preventDefault();
+      event.stopPropagation();
+      return false;
+    },
+
+    pos: function(event) {
+      var self = this;
+      var clientAxis = self.options.direction === 'vertical' ? 'clientY' : 'clientX';
+      // touch
+      if (event.targetTouches && (event.targetTouches.length >= 1)) {
+        return event.targetTouches[0][clientAxis];
+      }
+      // mouse
+      return event[clientAxis];
+    },
+
+    scroll: function(n) {
+      var self = this;
+
+      self.offset = (n > self.max) ? self.max : (n < self.min) ? self.min : n;
+
+      if (self.options.direction === 'vertical') {
+        self.element.style[csssupport('transform')] = 'translate3d(0,' + (-self.offset) + 'px,0)';
+      } else {
+        self.element.style[csssupport('transform')] = 'translate3d(' + (-self.offset) + 'px,0,0)';
+      }
     }
 
   };
