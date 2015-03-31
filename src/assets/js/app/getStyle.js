e2c22c5a202c0c2ffb2ac506d1eb77bb22ed0821
Nick Harris <nick@pixelenvy.co.uk>
2015-03-31 14:25:19 +0100

    Modified getStyle to handle any number of stylesheets

--- src/assets/js/app/getStyle.js
+++ src/assets/js/app/getStyle.js
@@ -1,18 +1,38 @@
 define(function(require){
-  function getStyle(className) {
-    var classes = document.styleSheets[1].rules || document.styleSheets[1].cssRules;
+  function cycleStylesheets(className) {
     var i = 0;
-    var n = classes.length;
+    var n = document.styleSheets.length;
+    var answer = null;
+
+    for (i; i<n; i++) {
+      answer = checkSheet(document.styleSheets[i], className) || answer;
+    }
+
+    return answer;
+  }
+
+  function checkSheet(stylesheet, className) {
+    var rules = stylesheet.rules || stylesheet.cssRules;
+    var answer;
+
+    if (rules) {
+      answer = getStyle(rules, className);
+      return answer;
+    }
+  }
+
+  function getStyle(rules, className) {
+    var i = 0;
+    var n = rules.length;
     var answer;
 
     for (i; i < n; i++) {
-      if (classes[i].selectorText == className) {
-        answer = classes[i].cssText ? classes[i].cssText : classes[i].style.cssText;
-        break;
+      if (rules[i].selectorText == className) {
+        answer = rules[i].cssText ? rules[i].cssText : rules[i].style.cssText;
       }
     }
     return answer;
   }
 
-  return getStyle;
+  return cycleStylesheets;
 });
