335f1faa4ec1102fdbe85fd63516bfabae5dd710
Nick Harris <nick@pixelenvy.co.uk>
2015-05-20 12:35:41 +0100

    Sass building all files in css root

--- tasks/sass.js
+++ tasks/sass.js
@@ -4,8 +4,11 @@ module.exports = {
       sourcemap: true
     },
     files: [{
-      '<%= package.paths.temp %>/css/<%= package.filenames.css %>.css': '<%= package.paths.assetsrc %>/css/main.scss',
-      '<%= package.paths.temp %>/css/dev.css': '<%= package.paths.assetsrc %>/css/dev.scss'
+      expand: true,
+      cwd: '<%= package.paths.assetsrc %>/css',
+      dest: '<%= package.paths.temp %>/css',
+      src: '*.scss',
+      ext: '.css'
     }]
   }
 };
