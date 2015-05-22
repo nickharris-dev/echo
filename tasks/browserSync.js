50a0ad8f4833ed4a09beced1342461d646393a59
Nick Harris <nick@pixelenvy.co.uk>
2015-05-22 20:11:35 +0100

    Removed browser setting from browsersync - it was opening it in parallels

--- tasks/browserSync.js
+++ tasks/browserSync.js
@@ -8,7 +8,6 @@ module.exports = function (grunt) {
       ]
     },
     options: {
-      browser: 'google chrome',
       server: {
         baseDir: '<%= package.paths.destinations.dev %>',
       },
