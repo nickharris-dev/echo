755a6b816a8f3dccf7e24774b9f9f352ea02d68e
Nick Harris <nick@pixelenvy.co.uk>
2015-06-05 12:20:47 +0100

    Added debounced/throttled scroll script

---
+++ src/assets/js/app/scroll.js
@@ -0,0 +1,33 @@
+define(['require'], function(){
+  // Throttle scroll
+
+  // Events
+  var scrollEnd = new Event('scrollEnd');
+  var debouncedScroll = new Event('debouncedScroll');
+  debouncedScroll.position = {};
+
+  // Helpers
+  var lastKnownY = 0;
+  var ticking = false;
+
+  function onScroll() {
+    lastKnownY = window.scrollY;
+    requestTick();
+  }
+
+  function requestTick() {
+    if (!ticking) {
+      requestAnimationFrame(update);
+    }
+    ticking = true;
+  }
+
+  function update() {
+    ticking = false;
+
+    debouncedScroll.position.y = lastKnownY;
+    document.dispatchEvent(debouncedScroll);
+  }
+
+  window.addEventListener('scroll', onScroll, false);
+});
