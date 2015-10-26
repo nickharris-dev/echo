cd9f1fc26329690fa4f6863851f71fbf8659df95
Nick Harris <nick@pixelenvy.co.uk>
2015-10-26 10:32:13 +0000

    Add crows navigation

--- src/theme/footer.php
+++ src/theme/footer.php
@@ -1,32 +1,13 @@
-			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+    <nav class="primary-nav" id="PrimaryNav">
+      <li class="primary-nav__home"><a href="/">Home</a></li>
+      <li><a href="/news">News</a></li>
+      <li><a href="/roster">Roster</a></li>
+    </nav>
 
-				<div id="inner-footer" class="wrap cf">
+		<footer role="contentinfo">
+      <p>Copyright© <?php echo date('Y'); ?> <a href="http://www.pixelenvy.co.uk">Pixel Envy Limited</a></p>
+    </footer>
 
-					<nav role="navigation">
-						<?php wp_nav_menu(array(
-    					'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
-    					'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
-    					'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
-    					'menu_class' => 'nav footer-nav cf',            // adding custom nav class
-    					'theme_location' => 'footer-links',             // where it's located in the theme
-    					'before' => '',                                 // before the menu
-    					'after' => '',                                  // after the menu
-    					'link_before' => '',                            // before each link
-    					'link_after' => '',                             // after each link
-    					'depth' => 0,                                   // limit the depth of the nav
-    					'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
-						)); ?>
-					</nav>
+  </body>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
-
-				</div>
-
-			</footer>
-
-		<?php // all js scripts are loaded in library/bones.php ?>
-		<?php wp_footer(); ?>
-
-	</body>
-
-</html> <!-- end of site. what a ride! -->
+</html>
