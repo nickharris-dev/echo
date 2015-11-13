79485732b20d508bbdc322492212c9a45e6346d9
Nick Harris <nick@pixelenvy.co.uk>
2015-11-13 23:41:00 +0000

    Player experience worked out from fixtures pages

--- src/theme/page-roster.php
+++ src/theme/page-roster.php
@@ -1,6 +1,6 @@
 <?php
 /*
- Template Name: Roster
+	Template Name: Roster
 */
 ?>
 
@@ -14,6 +14,7 @@
 
 	<?php
 		$terms = get_terms( 'Team' );
+		$today = new DateTime('now');
 		foreach( $terms as $term ) :
 	?>
 		<section class="team team--<?php print_r($term->slug); ?>">
@@ -23,7 +24,7 @@
 			<?php endif; ?>
 			<div class="team__wrapper">
 				<?php
-					$args = array(
+					$players = array(
 						'post_type' => 'player',
 						'meta_key' => 'shirt_number',
 						'orderby' => 'meta_value_num',
@@ -36,15 +37,44 @@
 							),
 						),
 					);
-					$posts = new WP_Query( $args );
+					$posts = new WP_Query( $players );
 					if( $posts->have_posts() ):
 						while( $posts->have_posts() ):
 							$posts->the_post();
 							$pic = get_field('photo')['sizes'];
-							$tz  = new DateTimeZone('Europe/Brussels');
-							$age = DateTime::createFromFormat('Ymd', get_field('date_of_birth'), $tz)
-					  	  ->diff(new DateTime('now', $tz))
+							$age = DateTime::createFromFormat('Ymd', get_field('date_of_birth'))
+					  	  ->diff($today)
 				    	  ->y;
+				    	$experience = 0;
+				    	$games_season = 0;
+				    	$games_career = 0;
+
+				    	$fixtures = get_posts(array(
+				    		'post_type' => 'fixture',
+				    		'meta_query' => array(
+				    			array(
+										'key' => 'roster',
+										'value' => '"' . get_the_ID() . '"',
+										'compare' => 'LIKE'
+				    			)
+				    		)
+				    	));
+
+				    	if ($fixtures) {
+				    		foreach ($fixtures as $fixture) {
+				    			$date = DateTime::createFromFormat('Ymd', get_field('date', $fixture));
+				    			if ($date < $today) {
+				    				$diff = $today->diff($date->sub(new DateInterval('P1Y')))->y;
+				    				if($diff > $experience) {
+				    					$experience = $diff;
+				    				}
+				    			}
+				    		}
+				    	}
+
+				    	if ($experience <=1) {
+				    		$experience = "Rookie";
+				    	}
 				?>
 					<article class="player">
 						<div class="player__wrapper">
@@ -70,16 +100,43 @@
 									<?php the_title(); ?>
 								</h1>
 							</div>
-							<table class="player__vitals">
+							<table cellpadding="0" cellspacing="0" class="player__vitals">
 								<tr>
 									<th scope="row">Position</th>
 									<td><?php echo strip_tags( get_the_term_list($post->ID, 'Position', '', ', ') ) ?></td>
 								</tr>
 								<tr>
 									<th scope="row">Age</th>
-									<td><?php echo $age ?></td>
+									<td><?php echo $age; ?></td>
+								</tr>
+								<tr>
+									<th scope="row">Experience</th>
+									<td><?php echo $experience; ?></td>
 								</tr>
 							</table>
+							<h2 class="player__stats__heading">Stats</h2>
+							<menu class="player__stats__tabs">
+								<li class="player__stats__tab player__stats__tab--active">Quick</li>
+								<li class="player__stats__tab">Career</li>
+							</menu>
+							<div class="player__stats">
+								<div class="player__stats__details player__stats__details--quick">
+									<table cellpadding="0" cellspacing="0">
+										<tr>
+											<th scope="rows">Games</th>
+											<td>?</td>
+										</tr>
+									</table>
+								</div>
+								<div class="player__stats__details player__stats__details--career">
+									<table cellpadding="0" cellspacing="0">
+										<tr>
+											<th scope="rows">Games</th>
+											<td>?</td>
+										</tr>
+									</table>
+								</div>
+							</div>
 						</div>
 					</article>
 		    <?php endwhile; endif; ?>
