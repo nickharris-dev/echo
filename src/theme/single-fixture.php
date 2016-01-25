d03ec65dbb23b2a785c44f69743d35de1cdc6dc1
Nick Harris <nick@pixelenvy.co.uk>
2016-01-25 08:24:09 +0000

    Start fixture page template output

--- src/theme/single-fixture.php
+++ src/theme/single-fixture.php
@@ -17,84 +17,76 @@
 
 <?php get_header(); ?>
 
-			<div id="content">
+	<article id="fixture-<?php the_ID(); ?>" class="fixture">
+		<header class="fixture__header">
+			<h1 class="fixture__title" itemprop="headline" rel="bookmark"><?php the_title(); ?></h1>
+		</header>
 
-				<div id="inner-content" class="wrap cf">
+		<section class="fixture__introduction">
+			<?php the_field('introduction'); ?>
+		</section>
 
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+		<?php if( have_rows('games') ): ?>
+		<section class="fixture__schedule">
+			<h2>Schedule</h2>
+			<table>
+				<thead>
+					<tr>
+						<th>Start Time</th>
+						<th>Home Team</th>
+						<th>Away Team</th>
+						<th>Officiating</th>
+					</tr>
+				</thead>
+			<?php while ( have_rows('games') ) : the_row(); ?>
+				<tr>
+					<td><?php if( get_sub_field('start_time') == 'TBC' ) ?><abbr class="acronym" title="To be confirmed">tbc</abbr></td>
+					<td>
+						<?php if( get_sub_field('responsibility') == 'the Home Team'): ?>
+							Manchester Crows
+						<?php else:
+							$teams = get_sub_field('home_team');
+							if( $teams ):
+								foreach( $teams as $post):
+									setup_postdata($post);?>
+										<?php the_title(); ?>
+							<?php endforeach; wp_reset_postdata(); endif; ?>
+						<?php endif; ?>
+					</td>
+					<td>
+						<?php if( get_sub_field('responsibility') == 'the Away Team'): ?>
+							Manchester Crows
+						<?php else:
+							$teams = get_sub_field('away_team');
+							if( $teams ):
+								foreach( $teams as $post):
+									setup_postdata($post);?>
+										<?php the_title(); ?>
+							<?php endforeach; wp_reset_postdata(); endif; ?>
+						<?php endif; ?>
+					</td>
+					<td>
+						<?php if( get_sub_field('responsibility') == 'Officiating'): ?>
+							Crows
+						<?php else:
+							 the_sub_field('officiating'); ?>
+						<?php endif; ?>
+					</td>
+				</tr>
+			<?php endwhile; ?>
+			</table>
+		</section>
+	<?php endif; ?>
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<section class="fixture__instructions">
+			<h2>Player Information</h2>
+			<?php the_field('player_instructions'); ?>
+		</section>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article">
-
-								<header class="article-header">
-
-									<h1 class="single-title custom-post-type-title"><?php the_title(); ?></h1>
-									<p class="byline vcard"><?php
-										printf( __( 'Posted <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time( 'Y-m-j' ), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) ), get_the_term_list( $post->ID, 'custom_cat', ' ', ', ', '' ) );
-									?></p>
-
-								</header>
-
-								<section class="entry-content cf">
-									<?php
-										// the content (pretty self explanatory huh)
-										the_content();
-
-										/*
-										 * Link Pages is used in case you have posts that are set to break into
-										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-										 *
-										*/
-										wp_link_pages( array(
-											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-											'after'       => '</div>',
-											'link_before' => '<span>',
-											'link_after'  => '</span>',
-										) );
-									?>
-								</section> <!-- end article section -->
-
-								<footer class="article-footer">
-									<p class="tags"><?php echo get_the_term_list( get_the_ID(), 'custom_tag', '<span class="tags-title">' . __( 'Custom Tags:', 'bonestheme' ) . '</span> ', ', ' ) ?></p>
-
-								</footer>
-
-								<?php comments_template(); ?>
-
-							</article>
-
-							<?php endwhile; ?>
-
-							<?php else : ?>
-
-									<article id="post-not-found" class="hentry cf">
-										<header class="article-header">
-											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-										<section class="entry-content">
-											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-											<p><?php _e( 'This is the error message in the single-custom_type.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
-
-						</main>
-
-						<?php get_sidebar(); ?>
-
-				</div>
-
-			</div>
+		<section class="fixture__spectators">
+			<h2>Spectators</h2>
+			<?php the_field('spectators'); ?>
+		</section>
+	</article>
 
 <?php get_footer(); ?>
