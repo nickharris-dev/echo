da40a8bf5739070c65577c92d5887a46237b7b8a
Nick Harris <nick@pixelenvy.co.uk>
2016-01-27 21:38:08 +0000

    Fixture styling

--- src/theme/single-fixture.php
+++ src/theme/single-fixture.php
@@ -19,13 +19,27 @@
 
 	<article id="fixture-<?php the_ID(); ?>" class="fixture">
 		<header class="fixture__header">
-			<h1 class="fixture__title" itemprop="headline" rel="bookmark"><?php the_title(); ?></h1>
-			<p class="fixture__date">
-				<?php
-					$date = DateTime::createFromFormat('Ymd', get_field('date'));
-					echo $date->format('D, jS F Y');
-				?>
-			</p>
+			<figure class="fixture__pic"><img
+			  <?php $pic = get_field('image')['sizes']; ?>
+			  alt="<?php print_r(get_field('image')['alt']); ?>"
+			  src="<?php print_r($pic['news_feature-1000']); ?>"
+			  srcset="
+			    <?php print_r($pic['news_feature-640']); ?> <?php print_r($pic['news_feature-640-width']); ?>w,
+			    <?php print_r($pic['news_feature-1000']); ?> <?php print_r($pic['news_feature-1000-width']); ?>w,
+			    <?php print_r($pic['news_feature-1500']); ?> <?php print_r($pic['news_feature-1500-width']); ?>w,
+			    <?php print_r($pic['news_feature-2000']); ?> <?php print_r($pic['news_feature-2000-width']); ?>w,
+			    <?php print_r($pic['news_feature-3000']); ?> <?php print_r($pic['news_feature-3000-width']); ?>w"
+			></figure>
+
+			<div class="fixture__hgroup" data-type-break="28rem">
+				<h1 class="fixture__title" itemprop="headline" rel="bookmark"><?php the_title(); ?></h1>
+				<p class="fixture__date">
+					<?php
+						$date = DateTime::createFromFormat('Ymd', get_field('date'));
+						echo $date->format('D, jS F Y');
+					?>
+				</p>
+			</div>
 		</header>
 
 		<main class="fixture__content">
@@ -47,7 +61,12 @@
 						</thead>
 					<?php while ( have_rows('games') ) : the_row(); ?>
 						<tr>
-							<td><?php if( get_sub_field('start_time') == 'TBC' ) ?><abbr class="acronym" title="To be confirmed">tbc</abbr></td>
+							<td>
+								<?php if( get_sub_field('start_time') == 'TBC' ): ?>
+									<abbr class="acronym" title="To be confirmed">tbc</abbr></td>
+								<?php else: ?>
+									<?php the_sub_field('start_time'); ?>
+								<?php endif; ?>
 							<td>
 								<?php if( get_sub_field('responsibility') == 'the Home Team'): ?>
 									<abbr title="Manchester Crows">Crows</abbr>
@@ -102,114 +121,114 @@
 				<h2>Spectators</h2>
 				<?php the_field('spectators'); ?>
 			</section>
-
-			<?php
-			  if( have_rows('location') ): ?>
-			  <section class="location location--fixture" id="Location">
-		      <?php while( have_rows('location') ): the_row();
-		        // vars
-		        $venue_name = get_sub_field('venue_name');
-		        $venue_address = get_sub_field('venue_address');
-		        $venue_postcode = get_sub_field('venue_postcode');
-		        $venue_lat = get_sub_field('venue_lat');
-		        $venue_lng = get_sub_field('venue_lng');
-		      ?>
-			      <article class="location__detail" data-lat="<?php echo $venue_lat; ?>" data-lng="<?php echo $venue_lng; ?>" id="Map<?php echo $count; ?>" data-eq="stacked:(min-width: 45rem)">
-			        <div class="location__info">
-		            <section class="location__location">
-				          <h1 class="location__venue"><?php echo $venue_name; ?></h1>
-				          <address>
-				            <?php echo $venue_address; ?>,
-				            <i class="postcode"><?php echo $venue_postcode; ?></i>
-				          </address>
-				          <h2>Directions</h2>
-				          <menu class="location__directions">
-				            <li class="location__trigger" data-type="geo">From where you are</li>
-				            <?php
-				              $trams = [];
-				              $trains = [];
-				              $buses = [];
-
-				              if( have_rows('public_transport_links') ):
-				                while ( have_rows('public_transport_links') ) : the_row();
-				                  $obj = new stdClass();
-				                  $obj->name = get_sub_field('name');
-				                  $obj->lat = get_sub_field('lat');
-				                  $obj->lng = get_sub_field('lng');
-
-				                  if( get_row_layout() == 'tram' ):
-				                    array_push($trams, $obj);
-				                  elseif( get_row_layout() == 'train' ):
-				                    array_push($trains, $obj);
-				                  elseif( get_row_layout() == 'bus' ):
-				                    array_push($buses, $obj);
-				                  endif;
-
-				                endwhile;
-				              endif;
-				            ?>
-
-			              <?php if (sizeof($trams) > 0): ?>
-				              <li class="location__trigger" data-locations="
-				                <?php for ($i = 0; $i < count($trams); $i++): ?>
-				                  <?php $tramstop = $trams[$i];
-				                    if ($i > 0): ?>,<?php endif;
-				                  ?>
-				                  {
-				                    'name': '<?php print_r ($tramstop->name); ?>',
-				                    'lat': '<?php print_r ($tramstop->lat); ?>',
-				                    'lng': '<?php print_r ($tramstop->lng); ?>'
-				                  }
-				                <?php endfor; ?>" data-type="tram">From the Nearest Tram Stop<?php if (count($trams) > 1): ?>s<?php endif; ?>
-				              </li>
-										<?php endif; ?>
-
-				            <?php if (sizeof($trains) > 0): ?>
-			                <li class="location__trigger" data-locations="
-				                <?php for ($i = 0; $i < count($trains); $i++): ?>
-				                  <?php $trainstation = $trains[$i];
-				                    if ($i > 0): ?>,<?php endif;
-				                  ?>
-				                  {
-				                    'name': '<?php print_r ($trainstation->name); ?>',
-				                    'lat': '<?php print_r ($trainstation->lat); ?>',
-				                    'lng': '<?php print_r ($trainstation->lng); ?>'
-				                  }
-				                <?php endfor; ?>" data-type="train">From the Nearest Train Station<?php if (count($trains) > 1): ?>s<?php endif; ?>
-				              </li>
-										<?php endif; ?>
-
-			              <?php if (sizeof($buses) > 0): ?>
-			                <li class="location__trigger" data-locations="
-				                <?php for ($i = 0; $i < count($buses); $i++): ?>
-				                  <?php $busstop = $buses[$i];
-				                    if ($i > 0): ?>,<?php endif;
-				                  ?>
-				                  {
-				                    'name': '<?php print_r ($busstop->name); ?>',
-				                    'lat': '<?php print_r ($busstop->lat); ?>',
-				                    'lng': '<?php print_r ($busstop->lng); ?>'
-				                  }
-				                <?php endfor; ?>" data-type="bus">From the Nearest Bus Stop<?php if (count($buses) > 1): ?>s<?php endif; ?>
-				              </li>
-				            <?php endif; ?>
-
-			              <li class="location__postcode" data-type="postcode">
-			                <form>
-			                  <input id="PostcodeInput" placeholder="From Postcode" type="text">
-			                  <button>&gt;</button>
-			                </form>
-			              </li>
-			            </menu>
-			            <button class="location__google">Open in Google Maps</button>
-		            </section>
-		          </div>
-		          <div class="location__map"></div>
-		        </article>
-					<?php endwhile; ?>
-			  </section>
-			<?php endif; ?>
 		</main>
+
+		<?php
+		  if( have_rows('location') ): ?>
+		  <section class="location location--fixture" id="Location">
+	      <?php while( have_rows('location') ): the_row();
+	        // vars
+	        $venue_name = get_sub_field('venue_name');
+	        $venue_address = get_sub_field('venue_address');
+	        $venue_postcode = get_sub_field('venue_postcode');
+	        $venue_lat = get_sub_field('venue_lat');
+	        $venue_lng = get_sub_field('venue_lng');
+	      ?>
+		      <article class="location__detail" data-lat="<?php echo $venue_lat; ?>" data-lng="<?php echo $venue_lng; ?>" id="Map<?php echo $count; ?>" data-eq="stacked:(min-width: 45rem)">
+		        <div class="location__info">
+	            <section class="location__location">
+			          <h1 class="location__venue"><?php echo $venue_name; ?></h1>
+			          <address>
+			            <?php echo $venue_address; ?>,
+			            <i class="postcode"><?php echo $venue_postcode; ?></i>
+			          </address>
+			          <h2>Directions</h2>
+			          <menu class="location__directions">
+			            <li class="location__trigger" data-type="geo">From where you are</li>
+			            <?php
+			              $trams = [];
+			              $trains = [];
+			              $buses = [];
+
+			              if( have_rows('public_transport_links') ):
+			                while ( have_rows('public_transport_links') ) : the_row();
+			                  $obj = new stdClass();
+			                  $obj->name = get_sub_field('name');
+			                  $obj->lat = get_sub_field('lat');
+			                  $obj->lng = get_sub_field('lng');
+
+			                  if( get_row_layout() == 'tram' ):
+			                    array_push($trams, $obj);
+			                  elseif( get_row_layout() == 'train' ):
+			                    array_push($trains, $obj);
+			                  elseif( get_row_layout() == 'bus' ):
+			                    array_push($buses, $obj);
+			                  endif;
+
+			                endwhile;
+			              endif;
+			            ?>
+
+		              <?php if (sizeof($trams) > 0): ?>
+			              <li class="location__trigger" data-locations="
+			                <?php for ($i = 0; $i < count($trams); $i++): ?>
+			                  <?php $tramstop = $trams[$i];
+			                    if ($i > 0): ?>,<?php endif;
+			                  ?>
+			                  {
+			                    'name': '<?php print_r ($tramstop->name); ?>',
+			                    'lat': '<?php print_r ($tramstop->lat); ?>',
+			                    'lng': '<?php print_r ($tramstop->lng); ?>'
+			                  }
+			                <?php endfor; ?>" data-type="tram">From the Nearest Tram Stop<?php if (count($trams) > 1): ?>s<?php endif; ?>
+			              </li>
+									<?php endif; ?>
+
+			            <?php if (sizeof($trains) > 0): ?>
+		                <li class="location__trigger" data-locations="
+			                <?php for ($i = 0; $i < count($trains); $i++): ?>
+			                  <?php $trainstation = $trains[$i];
+			                    if ($i > 0): ?>,<?php endif;
+			                  ?>
+			                  {
+			                    'name': '<?php print_r ($trainstation->name); ?>',
+			                    'lat': '<?php print_r ($trainstation->lat); ?>',
+			                    'lng': '<?php print_r ($trainstation->lng); ?>'
+			                  }
+			                <?php endfor; ?>" data-type="train">From the Nearest Train Station<?php if (count($trains) > 1): ?>s<?php endif; ?>
+			              </li>
+									<?php endif; ?>
+
+		              <?php if (sizeof($buses) > 0): ?>
+		                <li class="location__trigger" data-locations="
+			                <?php for ($i = 0; $i < count($buses); $i++): ?>
+			                  <?php $busstop = $buses[$i];
+			                    if ($i > 0): ?>,<?php endif;
+			                  ?>
+			                  {
+			                    'name': '<?php print_r ($busstop->name); ?>',
+			                    'lat': '<?php print_r ($busstop->lat); ?>',
+			                    'lng': '<?php print_r ($busstop->lng); ?>'
+			                  }
+			                <?php endfor; ?>" data-type="bus">From the Nearest Bus Stop<?php if (count($buses) > 1): ?>s<?php endif; ?>
+			              </li>
+			            <?php endif; ?>
+
+		              <li class="location__postcode" data-type="postcode">
+		                <form>
+		                  <input id="PostcodeInput" placeholder="From Postcode" type="text">
+		                  <button>&gt;</button>
+		                </form>
+		              </li>
+		            </menu>
+		            <button class="location__google">Open in Google Maps</button>
+	            </section>
+	          </div>
+	          <div class="location__map"></div>
+	        </article>
+				<?php endwhile; ?>
+		  </section>
+		<?php endif; ?>
 	</article>
 
 <?php get_footer(); ?>
