c22928079e184db00cce69272266370092a9f42e
Nick Harris <nick@pixelenvy.co.uk>
2016-02-13 17:57:00 +0000

    Final tweaks before pushing live for the first time

--- src/theme/footer.php
+++ src/theme/footer.php
@@ -12,8 +12,18 @@
       <li class="primary-nav__home"><a href="/">Home</a></li>
     </nav>
 
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCwRffT8sStG1kBc5v9u0QE10hcEAvK7dk"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB5eJGHGiEKPg9_QiWLNTtVk9zoKAKGzeQ"></script>
     <script src="<?php echo assets_url(); ?>/js/base.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-73756217-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
   </body>
 
 </html>
