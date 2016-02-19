4a0ba38a711ff601ce8e7b58b7c801d8cf7209e3
Nick Harris <nick@pixelenvy.co.uk>
2016-02-19 12:50:00 +0000

    Ommit google analytics on dev

--- src/theme/footer.php
+++ src/theme/footer.php
@@ -12,18 +12,25 @@
       <li class="primary-nav__home"><a href="/">Home</a></li>
     </nav>
 
+    <?php
+      $is_dev = false;
+      $url = site_url();
+      if ($url == 'http://local.wordpress.dev') $is_dev = true; ?>
+
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB5eJGHGiEKPg9_QiWLNTtVk9zoKAKGzeQ"></script>
     <script src="<?php echo assets_url(); ?>/js/base.1.0.2.js"></script>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    <?php if (!$is_dev): ?>
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-73756217-1', 'auto');
-      ga('send', 'pageview');
+        ga('create', 'UA-73756217-1', 'auto');
+        ga('send', 'pageview');
 
-    </script>
+      </script>
+    <?php endif; ?>
   </body>
 
 </html>
