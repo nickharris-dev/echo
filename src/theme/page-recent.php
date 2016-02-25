877bb45a03af6a0ce986072bee2600d67c5167ea
Nick Harris <nick@pixelenvy.co.uk>
2016-02-25 20:10:38 +0000

    Simplify recent posts

--- src/theme/page-recent.php
+++ src/theme/page-recent.php
@@ -13,62 +13,29 @@
     'posts_per_page' => 16
   );
   $posts = new WP_Query( $the_array );
-  if( $posts->have_posts() ):
-    $counter = 0; ?>
-    <section class="posts" id="RecentNews">
+  if( $posts->have_posts() ): ?>
+    <section class="posts posts--recent">
     <?php while( $posts->have_posts() ):
-      $posts->the_post();
-      $pic = get_field('image')['sizes']; ?>
-      <?php if ($counter == 1) :?>
-        <div class="posts__wrapper">
-      <?php endif; ?>
+      $posts->the_post(); ?>
       <article class="posts__post">
         <div class="posts__post__copy">
           <h1 class="posts__post__heading">
             <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
           </h1>
-          <p>
+          <p class="posts__post__excerpt">
             <?php the_field('excerpt'); ?>
           </p>
         </div>
-        <figure class="posts__post__pic">
-          <?php if($counter < 1) : ?>
-            <img
-              alt="<?php print_r(get_field('image')['alt']); ?>"
-              src="<?php print_r($pic['news_feature-1000']); ?>"
-              srcset="
-                <?php print_r($pic['news_feature-640']); ?> <?php print_r($pic['news_feature-640-width']); ?>w,
-                <?php print_r($pic['news_feature-1000']); ?> <?php print_r($pic['news_feature-1000-width']); ?>w,
-                <?php print_r($pic['news_feature-1500']); ?> <?php print_r($pic['news_feature-1500-width']); ?>w,
-                <?php print_r($pic['news_feature-2000']); ?> <?php print_r($pic['news_feature-2000-width']); ?>w,
-                <?php print_r($pic['news_feature-3000']); ?> <?php print_r($pic['news_feature-3000-width']); ?>w"
-            >
-           <?php else: ?>
-            <a href="<?php the_permalink(); ?>"><img
-              alt="<?php print_r(get_field('image')['alt']); ?>"
-              src="<?php print_r($pic['square-300']); ?>"
-              srcset="
-                <?php print_r($pic['square-150']); ?> <?php print_r($pic['square-150-width']); ?>w,
-                <?php print_r($pic['square-300']); ?> <?php print_r($pic['square-300-width']); ?>w,
-                <?php print_r($pic['square-450']); ?> <?php print_r($pic['square-450-width']); ?>w,
-                <?php print_r($pic['square-600']); ?> <?php print_r($pic['square-600-width']); ?>w,
-                <?php print_r($pic['square-750']); ?> <?php print_r($pic['square-750-width']); ?>w,
-                <?php print_r($pic['square-900']); ?> <?php print_r($pic['square-900-width']); ?>w,
-                <?php print_r($pic['square-1050']); ?> <?php print_r($pic['square-1050-width']); ?>w,
-                <?php print_r($pic['square-1200']); ?> <?php print_r($pic['square-1200-width']); ?>w
-                "
-              sizes="(min-width: 56.25em) 11.111vw,
-                     (min-width: 37.5em) 16.65vw,
-                     33.333vw"
-            ></a>
-           <?php endif; ?>
-         </figure>
+        <?php if ( have_rows('hero') ) :
+          while ( have_rows('hero') ) : the_row(); ?>
+          <a href="<?php the_permalink(); ?>" class="posts__post__pic" <?php if( get_row_layout() === 'image'):
+            smart_media_image(get_sub_field('image'), get_sub_field('focal_point_x'), get_sub_field('focal_point_y'), 'hero');
+          endif; ?>></a>
+        <?php endwhile; endif; ?>
       </article>
-    <?php $counter++;
-      endwhile; ?>
-        </div>
+    <?php endwhile; ?>
     </section>
-<?php endif; ?>
+<?php endif; wp_reset_query(); ?>
 
 <?php include 'training.php'; ?>
 
